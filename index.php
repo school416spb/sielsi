1bc0a3cddbfb9b2128b4914e7511e0777008148a
Денис Давыдов <46064010+school416spb@users.noreply.github.com>
2021-12-30 14:16:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -79,20 +79,6 @@
 				</div>
 			</div>
 		</div>
-		
-		
-		<div class="row justify-content-sm-center" style="margin-top: 50px;">
-		<div class="col-12 col-sm-9 col-md-7 col-lg-5">
-			 
-		    <div class="alert alert-warning" role="alert">
-              Уважаемые пользователи! Теперь в качестве логина для входа в систему используется ИНН организации, пароли остались прежние! 
-              В случае возниконовения сложностей или при нахождении программных ошибок обращайтесь на почту support@school416spb.ru
-            </div>	 
-			    
-		</div>
-		</div
-		
-		
 	</div>
 </body>
 </html>
