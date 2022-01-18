1456454cdb783921b6f99a631aafe85e86eb00d3
Денис Давыдов <46064010+school416spb@users.noreply.github.com>
2022-01-18 11:38:54 +0300

    Update main.php

--- resources/main.php
+++ resources/main.php
@@ -49,7 +49,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Электронная подпись документа</title>
+    <title>Простая электронная подпись</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css?<?php echo time(); ?>">
     <link rel="shortcut icon" href="../img/favicon.ico">
