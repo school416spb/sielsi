8f92a2664fc55c3dd6b55eafc036912f422b6ca8
Денис Давыдов <46064010+school416spb@users.noreply.github.com>
2022-01-18 10:45:39 +0300

    Update version.php

--- templates/version.php
+++ templates/version.php
@@ -1,2 +1,3 @@
 <small class="text-secondary">2021 <i class="fa fa-copyright" aria-hidden="true"></i> Давыдов Д.Э.<br>
-версия 2.21.12.31</small>
+davydov@school416spb.ru<br>
+версия 2.21.1.18</small>
